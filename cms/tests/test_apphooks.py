f9b1fc4ae8e010f8771fce09e858551a8bdca92c
Nicolas PASCAL <np.pascal@gmail.com>
2019-08-31 18:03:29 +0200

    Update test_apphooks.py

--- cms/tests/test_apphooks.py
+++ cms/tests/test_apphooks.py
@@ -1094,18 +1094,20 @@ class ApphooksPageLanguageUrlTestCase(CMSTestCase):
         
         #request._current_page_cache = applications_page_check(request)
         from cms.utils.moderator import use_draft
-        use_draft(request)        
-      #  request._current_page_cache = applications_page_check(request)
+      
+        request._current_page_cache = applications_page_check(request)
         page = get_page_from_request(request, clean_path=True)
         
        # self.assertEqual( path, '/PREFIX/en/child_page/child_child_page/extra_1/')
         #page was a finded with  '/PREFIX/en/child_page/child_child_page/extra_1/' 
         self.assertEqual(page.get_absolute_url(), '/en/child_page/child_child_page/extra_1/')
-      
+        
+        """
         request.LANGUAGE_CODE = 'de'
-        request._current_page_cache = applications_page_check(request)
+      #  request._current_page_cache = applications_page_check(request)
         page = get_page_from_request(request)
         
         self.assertEqual( path, '/PREFIX/en/child_page/child_child_page/extra_1')
         
         self.assertEqual(page.get_absolute_url(), '/PREFIX/de/child_page/child_child_page/')
+        """
